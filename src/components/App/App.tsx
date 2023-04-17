9edd246dd2ee2faf5bfda653383295ea5a495d54
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-17 19:39:15 +0200

    Initial commit. Setting up project environment.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { AuthProvider } from '../../context/AuthProvider';
+import { Auth } from '../Auth/Auth';
+// import { RequireAuth } from '../RequireAuth/RequireAuth';
+
+export const App = () => {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="login" element={<Auth />} />
+        <Route path="register" element={<Auth />} />
+        <Route path="/" element={<h1>MegaK - projekt finałowy</h1>} />
+        {/* <Route path="error" element={} /> */}
+        {/* <Route path="*" element={} /> */}
+        {/* <Route path="404" element={} /> */}
+        {/* <Route path="/" element={<RequireAuth />}> */}
+        {/* <Route path="settings" element={}> */}
+        {/*  <Route path="email" element={} /> */}
+        {/*  <Route path="password" element={} /> */}
+        {/* </Route> */}
+        {/* <Route path="/" element={} /> */}
+        {/* </Route> */}
+      </Routes>
+    </AuthProvider>
+  );
+};
