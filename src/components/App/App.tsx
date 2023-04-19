125a39a5c9207b575a4df90a5ae151466bf2d29f
Jakub <jakub.gurec@gmail.com>
2023-04-19 22:30:11 +0200

    Added global styles, style for primary button, and variable font

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,7 +11,17 @@ export const App = () => {
       <Routes>
         <Route path="login" element={<Auth />} />
         <Route path="register" element={<Auth />} />
-        <Route path="/" element={<h1>MegaK - projekt finałowy</h1>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>MegaK - projekt finałowy</h1>
+              <button type="button" className="primary-btn">
+                Zaloguj się
+              </button>
+            </>
+          }
+        />
         {/* <Route path="error" element={} /> */}
         {/* <Route path="*" element={} /> */}
         {/* <Route path="404" element={} /> */}
