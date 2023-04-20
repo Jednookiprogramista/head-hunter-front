b46d1fdd095a04ff2a5c9c5ded96b6824f5a8e3d
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-20 14:40:49 +0200

    Added Login and Register form. Added basic styling for Auth components.

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,5 +1,94 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Button, TextField, Typography } from '@mui/material';
+import { axiosPrivate } from '../../../api/axios';
+import { useAuth } from '../../../hooks/useAuth';
+import '../Auth.css';
 
 export const Login = () => {
-  return <h1>Zaloguj się</h1>;
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = (location?.state as { from: string })?.from || '/';
+
+  useEffect(() => {
+    setError('');
+  }, [email, password]);
+
+  useEffect(() => {
+    if (auth) {
+      navigate(from);
+    }
+  }, [auth, from, navigate]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      if (!email || !password) {
+        return;
+      }
+      setLoading(true);
+      const { data } = (await axiosPrivate.post('session', {
+        email,
+        password,
+      })) as { data: { id: string; accessToken: string } };
+      localStorage.setItem('user', JSON.stringify({ id: data.id }));
+      setAuth(data);
+      setLoading(false);
+      navigate('/');
+    } catch (err: any) {
+      const message = 'Nieprawidłowy e-mail lub hasło.';
+      setError(message);
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <p>ładowanie</p>;
+
+  return (
+    <form className="Auth__form" onSubmit={handleSubmit}>
+      <TextField
+        label="E-mail"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Hasło"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        margin="normal"
+        fullWidth
+      />
+      <Typography variant="body2" className="forgot-pass">
+        <Link to="/register">Zapomniałeś hasła?</Link>
+      </Typography>
+      <Button
+        type="submit"
+        variant="contained"
+        disabled={!email || !password || !!error}
+        fullWidth
+      >
+        Zaloguj się
+      </Button>
+      {error && (
+        <Typography variant="body2" color="error" gutterBottom>
+          {error}
+        </Typography>
+      )}
+      <Typography variant="body2" className="redirect-paraph">
+        Nie masz konta? <Link to="/register">Zarejestruj się</Link>
+      </Typography>
+    </form>
+  );
 };
--- src/components/Auth/Register/Register.tsx
+++ src/components/Auth/Register/Register.tsx
@@ -1,5 +1,119 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, TextField, Typography } from '@mui/material';
+import { useUserValidation } from '../../../hooks/useRegisterValidation';
+import { axios } from '../../../api/axios';
 
 export const Register = () => {
-  return <h1>Zarejestruj się</h1>;
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepetition, setPasswordRepetition] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const loginLinkRef = useRef<HTMLAnchorElement>(null!);
+  const { emailError, passwordError, passwordRepetitionError } =
+    useUserValidation({ email, password, passwordRepetition });
+
+  useEffect(() => {
+    setError('');
+  }, [email, password, passwordRepetition]);
+
+  useEffect(() => {
+    if (success) {
+      loginLinkRef.current.focus();
+    }
+  }, [success]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      if (
+        !email ||
+        !password ||
+        !passwordRepetition ||
+        emailError ||
+        passwordError ||
+        passwordRepetitionError
+      ) {
+        return;
+      }
+      setLoading(true);
+      await axios.post('user', { email, password });
+      setError('');
+      setPassword('');
+      setPasswordRepetition('');
+      setSuccess(true);
+    } catch (err: any) {
+      if (err.response.status === 409) {
+        const message =
+          'Podany adres email jest już zajęty, jeśli masz już konto zaloguj się.';
+        setError(message);
+      } else {
+        const message =
+          'Wystąpił nieoczekiwany błąd. Spróbuj ponownie później.';
+        setError(message);
+      }
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <p>ładowanie</p>;
+
+  return (
+    <form className="Auth__form" onSubmit={handleSubmit}>
+      <h2>Zarejestruj się</h2>
+      <TextField
+        label="Adres email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        error={emailError}
+        helperText={emailError}
+        required
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Hasło"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        error={passwordError}
+        helperText={passwordError}
+        required
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Powtórz hasło"
+        type="password"
+        value={passwordRepetition}
+        onChange={(e) => setPasswordRepetition(e.target.value)}
+        error={passwordRepetitionError}
+        helperText={passwordRepetitionError}
+        required
+        fullWidth
+        margin="normal"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        disabled={!email || !password || !passwordRepetition || !!error}
+        fullWidth
+      >
+        Zarejestruj się
+      </Button>
+      {error && (
+        <Typography variant="body2" color="error" gutterBottom>
+          {error}
+        </Typography>
+      )}
+      <Typography variant="body2" className="redirect-paraph">
+        Posiadasz już konto? <Link to="/login">Zaloguj się</Link>
+      </Typography>
+    </form>
+  );
 };
