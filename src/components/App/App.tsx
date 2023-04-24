34542dc7d0ac2d02320e92a7ff5a91b41073fe1b
Your Name <bvbrunik@gmail.com>
2023-04-24 18:33:05 +0200

    fixed code in app.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,38 +2,40 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from '../../context/AuthProvider';
 import { Auth } from '../Auth/Auth';
-import { Button } from '../Button/Button';
+// import { Button } from '../Button/Button';
 import { List } from '../layout/list/List';
 import { ListConversation } from '../layout/listConversation/ListConversation';
 // import { RequireAuth } from '../RequireAuth/RequireAuth';
 
 export const App = () => {
-  return (
-    <AuthProvider>
-      <Routes>
-        <Route path="login" element={<Auth />} />
-        <Route path="register" element={<Auth />} />
+    return (
+        <AuthProvider>
+            <Routes>
+                <Route path="login" element={<Auth />} />
+                <Route path="register" element={<Auth />} />
 
-        <Route path="list" element={<List />} />
-        <Route path="conversation" element={<ListConversation />} />
+                <Route path="list" element={<List />} />
+                <Route path="conversation" element={<ListConversation />} />
 
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>MegaK - projekt finałowy</h1>
-
-        {/* <Route path="error" element={} /> */}
-        {/* <Route path="*" element={} /> */}
-        {/* <Route path="404" element={} /> */}
-        {/* <Route path="/" element={<RequireAuth />}> */}
-        {/* <Route path="settings" element={}> */}
-        {/*  <Route path="email" element={} /> */}
-        {/*  <Route path="password" element={} /> */}
-        {/* </Route> */}
-        {/* <Route path="/" element={} /> */}
-        {/* </Route> */}
-      </Routes>
-    </AuthProvider>
-  );
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <h1>MegaK - projekt finałowy</h1>
+                            {/* <Route path="error" element={<Error />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="404" element={<NotFound />} />
+              <Route path="/" element={<RequireAuth />}>
+                <Route path="settings" element={<Settings />}>
+                  <Route path="email" element={<Email />} />
+                  <Route path="password" element={<Password />} />
+                </Route>
+                <Route path="/" element={<Home />} />
+              </Route> */}
+                        </>
+                    }
+                />
+            </Routes>
+        </AuthProvider>
+    );
 };
